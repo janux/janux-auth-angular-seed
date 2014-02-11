d4dc7b12f17b12318a68caac560a7bb14f6f3152
Philippe Paravicini <pparavicini@janux.org>
2014-02-11 10:36:35 -0800

    add initial janux branding, remove project management links, add home page placeholder

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -35,7 +35,13 @@ angular.module('app').constant('I18N.MESSAGES', {
 
 angular.module('app').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
-  $routeProvider.otherwise({redirectTo:'/projectsinfo'});
+
+  $routeProvider
+		.when('/home', {
+			templateUrl: 'home.tpl.html'
+		})
+		.otherwise({redirectTo:'/home'});
+  // $routeProvider.otherwise({redirectTo:'/projectsinfo'});
 }]);
 
 angular.module('app').run(['security', function(security) {
@@ -69,7 +75,7 @@ angular.module('app').controller('HeaderCtrl', ['$scope', '$location', '$route',
     if (security.isAuthenticated()) {
       $location.path('/dashboard');
     } else {
-      $location.path('/projectsinfo');
+      $location.path('/home');
     }
   };
 

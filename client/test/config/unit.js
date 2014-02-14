74a81c5ff5296a443c83afd57f27264e4c8cd52f
Philippe Paravicini <pparavicini@janux.org>
2014-02-13 17:33:11 -0800

    - Add file logger for tests - Change pattern layout of console logger to make it more concise - Upgrade grunt-karma to 0.5 and karma to 0.9.x to be able to integrate PhantomJS - Integrate PhantomJS to prevent browser screen to be launched on every test run

--- client/test/config/unit.js
+++ client/test/config/unit.js
@@ -1,57 +1,87 @@
-// base path, that will be used to resolve files and exclude
-basePath = '../..';
+module.exports = function(karma) {
+	karma.set({
+		// base path, that will be used to resolve files and exclude
+		basePath: '../..',
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'vendor/jquery/jquery.js',
-  'vendor/angular/angular.js',
-  'vendor/angular/angular-route.js',
-  'vendor/mongolab/mongolab-resource.js',
-  'test/vendor/angular/angular-mocks.js',
-  'vendor/angular-ui/**/*.js',
-  'src/**/*.js',
-  'test/unit/**/*.spec.js',
-  'dist/templates/**/*.js'
-];
+		frameworks: ['jasmine'],
 
-// use dots reporter, as travis terminal does not support escaping sequences
-// possible values: 'dots' || 'progress'
-reporters = 'progress';
 
-// these are default values, just to show available options
+		// list of files / patterns to load in the browser
+		files: [
+			'vendor/jquery/jquery.js',
+			'vendor/angular/angular.js',
+			'vendor/angular/angular-route.js',
+			'vendor/mongolab/mongolab-resource.js',
+			'test/vendor/angular/angular-mocks.js',
+			'vendor/angular-ui/**/*.js',
+			'src/**/*.js',
+			'test/unit/**/*.spec.js',
+			'dist/templates/**/*.js'
+		],
 
-// web server port
-port = 8089;
+		// use dots reporter, as travis terminal does not support escaping sequences
+		// possible values: 'dots' || 'progress' || junit
+		reporters: ['dots','junit'],
+		// reporters: 'junit',
 
-// cli runner port
-runnerPort = 9109;
+		junitReporter: {
+			outputFile: 'test.xml',
+			suite: 'unit'
+		},
 
-urlRoot = '/__test/';
+		// web server port
+		port: 8089,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+		// cli runner port
+		runnerPort: 9109,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+		urlRoot: '/__test/',
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
-// polling interval in ms (ignored on OS that support inotify)
-autoWatchInterval = 0;
+		// level of logging
+		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+		logLevel: karma.LOG_INFO,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari
-// - PhantomJS
-browsers = ['Chrome'];
+		// see log4js configuration for 'appenders'
+		loggers: [
+			{ 
+				type: 'file',
+				filename: 'test.log',
+				layout : {
+					type : 'pattern',
+					pattern : '%d | %p | %c | %m'
+				}
+			},
+			{
+				type: 'console',
+				layout: {
+					type : 'pattern',
+					pattern : '%[%d | %p | %c |%] %m'
+				}
+			}
+		],
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = true;
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: false,
+
+		// polling interval in ms (ignored on OS that support inotify)
+		autoWatchInterval: 0,
+
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari
+		// - PhantomJS
+		// browsers: ['Chrome'],
+		// browsers: ['PhantomJS', 'Chrome'],
+		browsers: ['PhantomJS'],
+
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun: true
+	});
+};

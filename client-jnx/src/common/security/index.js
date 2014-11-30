1fa719372939ac94cf6e8180cf73124229a9fbc5
Philippe Paravicini <philippe@paravicini.com>
2014-11-29 17:53:32 -0800

    rename security-factory.js to security-service.js

--- client-jnx/src/common/security/index.js
+++ client-jnx/src/common/security/index.js
@@ -3,6 +3,6 @@
 require('ui-bootstrap-modal');
 
 require('angular').module('security', ['ui.bootstrap.dialog'])
-	.factory(   'security',            require('./security-factory.js'))
+	.factory(   'security',            require('./security-service.js'))
 	.directive( 'loginToolbar',        require('./login/toolbar.js'))
 	.controller('LoginFormController', require('./login/login-form-controller.js'))

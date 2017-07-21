01891aaf0b6dcaef17bfaaa4335a1593a9e518c9
ernestoort <ernesto.tito@gail.com>
2017-07-21 11:09:39 -0500

    Added a comment and resolve some referecnes.

--- server/src/api/daos.js
+++ server/src/api/daos.js
@@ -1,14 +1,16 @@
 'use strict';
 
 var config            = require('config').serverAppContext,
-	promise           = require('bluebird'),
 	DataSourceHandler = require('janux-persistence').DataSourceHandler,
 	DaoFactory        = require('janux-persistence').DaoFactory;
 
- module.exports = {
- 	accountDaoLokijs: require('janux-persistence').DaoFactory.createAccountDao(DataSourceHandler.LOKIJS, config.db.lokiJsDBPath),
- 	partyDaoLokijs: require('janux-persistence').DaoFactory.createPartyDao(DataSourceHandler.LOKIJS, config.db.lokiJsDBPath),
- 	accountDaoMongo: require('janux-persistence').DaoFactory.createAccountDao(DataSourceHandler.MONGODB, config.db.mongoConnUrl),
- 	partyDaoMongo: require('janux-persistence').DaoFactory.createPartyDao(DataSourceHandler.MONGODB, config.db.mongoConnUrl),
- 	authDAO: require('./authorization-dao').object()
- };
+// When creating a dao also creates a db connection to the database.
+// In case you want the program do not open unnecessary connections, comment the daos
+// you don't need and make sure the commented daos are not referenced in the config settings.
+module.exports = {
+	accountDaoLokijs: DaoFactory.createAccountDao(DataSourceHandler.LOKIJS, config.db.lokiJsDBPath),
+	partyDaoLokijs: DaoFactory.createPartyDao(DataSourceHandler.LOKIJS, config.db.lokiJsDBPath),
+	accountDaoMongo: DaoFactory.createAccountDao(DataSourceHandler.MONGODB, config.db.mongoConnUrl),
+	partyDaoMongo: DaoFactory.createPartyDao(DataSourceHandler.MONGODB, config.db.mongoConnUrl),
+	authDAO: require('./authorization-dao').object()
+};

aac1150ae21f0361ba1b9a36f418f5813f751e2c
Alfredo Alejandro Alvarez Ortiz <alejandro@januxmedia.com>
2017-08-23 21:00:28 -0500

    roles as an array for the drag-and-drop list

--- client/src/app/role/roles-controller.js
+++ client/src/app/role/roles-controller.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = [
 '$scope','roles', function($scope , roles) {
-	$scope.roles = roles;
-	// console.log('Loaded roles', roles);
+	$scope.roles = _.values(roles);
+	// console.log('Loaded roles', $scope.roles);
 
 	$scope.deleteRole = function () {
 		// TODO: Implement functionality to delete a role

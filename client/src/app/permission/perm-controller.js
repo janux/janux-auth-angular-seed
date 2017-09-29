c96301ca19e143949f9dd62ee4c9e585f5b3158d
Alfredo Alejandro Alvarez Ortiz <alejandro@januxmedia.com>
2017-09-29 18:27:05 -0500

    Confirmation dialog when deleting an authorization context

--- client/src/app/permission/perm-controller.js
+++ client/src/app/permission/perm-controller.js
@@ -1,16 +1,43 @@
 'use strict';
 
 module.exports = [
-'$scope','authContextGroups','authService','$state', function(
- $scope , authContextGroups , authService , $state) {
+'$scope','authContextGroups','authService','$state','$modal', function(
+ $scope , authContextGroups , authService , $state , $modal) {
 
-		$scope.authContextGroups = authContextGroups;
-		// console.log('authContextGroups', authContextGroups);
+	$scope.authContextGroups = authContextGroups;
+	// console.log('authContextGroups', authContextGroups);
 
-		$scope.deleteAuthContext = function (groupCode, name) {
-			return 	authService.deleteAuthorizationContextByName(groupCode, name)
-				.then(function () {
-					$state.go($state.current, {}, {reload: true});
-				});
+	var deleteAuthContext = function (groupCode, name) {
+		return 	authService.deleteAuthorizationContextByName(groupCode, name)
+			.then(function () {
+				$state.go($state.current, {}, {reload: true});
+			});
+	};
+
+	var deleteWidgetCtrl = ['$scope','$modalInstance', 'data',
+					function($scope , $modalInstance, data) {
+		$scope.targetDescr = 'Authorization Context: '+data.name;
+
+		$scope.ok = function() {
+			deleteAuthContext(data.groupCode, data.name);
+			$modalInstance.close();
 		};
+
+		$scope.cancel = function() {
+			$modalInstance.dismiss();
+		};
+	}];
+
+	$scope.openDeleteAuthContextDialog = function(groupCode, name){
+		$modal.open({
+			templateUrl: 'app/dialog-tpl/delete-dialog.html',
+			controller: deleteWidgetCtrl,
+			size: 'md',
+			resolve: {
+				data: function () {
+					return { groupCode:groupCode, name:name };
+				}
+			}
+		});
+	};
 }];

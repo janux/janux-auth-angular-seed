18fb5ed0771d7ee52dd03683894b90ff31ea9787
Alfredo Alejandro Alvarez Ortiz <alejandro@januxmedia.com>
2017-10-23 21:24:40 -0500

    ag-grid row actions component

--- client/src/common/ag-grid-components/index.js
+++ client/src/common/ag-grid-components/index.js
@@ -2,8 +2,9 @@
 
 module.exports = {
 	// Custom Cell Editors
-	simpleCellEditor: require('./simple-cell-editor'),
-	dateCellEditor: require('./date-cell-editor'),
+	simpleCellEditor: 	require('./simple-cell-editor'),
+	dateCellEditor: 	require('./date-cell-editor'),
+	rowActions: 		require('./row-actions-component'),
 
 	// Custom Cell Renderer
 	simpleCellRenderer: require('./simple-cell-renderer'),

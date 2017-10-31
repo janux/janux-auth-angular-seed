800cad353105eff8eeab60e320f36efa44ccb44c
ernestoort <ernesto.tito@gail.com>
2017-10-31 16:07:58 -0600

    glarus services client integration.
    What's next?  Put some data to se the json response in the browser.

--- client/src/common/demo-service/index.js
+++ client/src/common/demo-service/index.js
@@ -5,6 +5,7 @@ require('angular').module('demoService', [])
 .factory('userService', require('./user-service.js'))
 .factory('authContextService', require('./auth-context-service.js'))
 .factory('roleService', require('./role-service.js'))
+.factory('operationService',require('./operation-service'))
 .run(function() {
 	console.log('janux auth seed correctly instantiated services');
 });

3654b203ab7068d809a4d2d35c35451dd6cb2f2b
Alfredo Alejandro Alvarez Ortiz <alejandro@januxmedia.com>
2017-11-07 16:56:36 -0600

    Implementing operations section

--- client/src/app/driver-logbook/mock-data.js
+++ client/src/app/driver-logbook/mock-data.js
@@ -1,46 +1,44 @@
 'use strict';
 
-module.exports = [
-	{
-		Personal: 'Cadena, German',
-		Service: 'Honda',
-		Start: '01/09/17 2:00 pm',
-		End: '01/09/17 3:00 pm',
-		Duration: '1 hrs.',
-		Provider: 'Glarus',
-		Location: '',
-		Absense: 'D',
-		Object: {
-			a: 'A',
-			b: 'B'
+module.exports = {
+	persons: [
+		{
+			'displayName': 'Gilberto Hernandez',
+			'name': {
+				'honorificPrefix': 'Mr.',
+				'first': 'Gilberto',
+				'middle': '',
+				'last': 'Hernandez',
+				'honorificSuffix': 'Sr.'
+			},
+			'typeName': 'PersonImpl',
+			'id': 'b1cbaaf7-bd66-4918-8130-7824b95c37e4',
+			'dateCreated': '2017-09-18T15:22:55.246Z'
+		},
+		{
+			'displayName': 'German Cadena',
+			'name': {
+				'honorificPrefix': 'Mr.',
+				'first': 'German',
+				'middle': '',
+				'last': 'Cadena',
+				'honorificSuffix': 'DDS'
+			},
+			'typeName': 'PersonImpl',
+			'id': 'c31361b1-71b2-4852-8508-c29cd84f3686',
+			'dateCreated': '2017-09-18T15:22:55.250Z'
+		},
+		{
+			'displayName': 'Harold Ramis',
+			'name': {
+				'honorificPrefix': 'General',
+				'first': 'Harold',
+				'last': 'Ramis',
+				'honorificSuffix': 'Prisioner'
+			},
+			'typeName': 'PersonImpl',
+			'id': '8294520d-fdab-45ce-89fa-a1ca66b7c78a',
+			'lastUpdate': '2017-10-20T17:29:51.439Z'
 		}
-	},
-	{
-		Personal: 'Hernandez, Gilberto',
-		Service: 'Honda',
-		Start:'01/09/17 10:00 am',
-		End: '01/09/17 3:00 pm',
-		Duration: '5 hrs.',
-		Provider: 'Glarus',
-		Location: '',
-		Absense: 'D',
-		Object: {
-			a: 'A',
-			b: 'B'
-		}
-	},
-	{
-		Personal: 'Martin, Perez',
-		Service: 'Honda',
-		Start:'05/09/17 2:00 pm',
-		End: '05/09/17 3:00 pm',
-		Duration: '1 hrs.',
-		Provider: 'Glarus',
-		Location: '',
-		Absense: 'D',
-		Object: {
-			a: 'A',
-			b: 'B'
-		}
-	}
-];
+	]
+};

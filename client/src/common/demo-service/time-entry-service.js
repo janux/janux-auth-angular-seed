d89dbe0709bac4bee889577357e077f3fc007e51
Ernesto Ortiz <ernesto.tito@gail.com>
2017-11-23 12:16:04 -0600

    Added time entry update method as json rpc method.

--- client/src/common/demo-service/time-entry-service.js
+++ client/src/common/demo-service/time-entry-service.js
@@ -1,5 +1,5 @@
 /**
- * Project janux-auth-angular-seed 
+ * Project janux-auth-angular-seed
  * Created by ernesto on 11/21/17.
  */
 
@@ -7,18 +7,27 @@
 var _ = require('lodash');
 
 module.exports =
-    ['$q', '$http',
-        function ($q, $http) {
-            var service = {
-                insert: function (timeEntry) {
-                    return $http.jsonrpc(
-                        '/rpc/2.0/timeEntry',
-                        'insert',
-                        [timeEntry]
-                    ).then( function (resp) {
-                        return resp.data.result;
-                    });
-                }
-            };
-            return service;
-        }];
+	['$q', '$http',
+		function ($q, $http) {
+			var service = {
+				insert: function (timeEntry) {
+					return $http.jsonrpc(
+						'/rpc/2.0/timeEntry',
+						'insert',
+						[timeEntry]
+					).then(function (resp) {
+						return resp.data.result;
+					});
+				},
+				update: function (timeEntry) {
+					return $http.jsonrpc(
+						'/rpc/2.0/timeEntry',
+						'update',
+						[timeEntry]
+					).then(function (resp) {
+						return resp.data.result;
+					});
+				}
+			};
+			return service;
+		}];

5c3be2694e03562bbe7649814a9d67f829ede51e
Alfredo Alejandro Alvarez Ortiz <alejandro@januxmedia.com>
2017-11-23 19:17:39 -0600

    Driver time sheet insert

--- client/src/app/driver-logbook/index.js
+++ client/src/app/driver-logbook/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('lodash');
+
 var angular= require('angular');
 // ECMA 5 - using nodes require() method
 var agGrid = require('ag-grid');
@@ -15,7 +17,22 @@ require('angular').module('appDriverLogbook', [
 		url:'/driver-logbook',
 		templateUrl: 'app/driver-logbook/index.html',
 		authRequired: true,
-		resolve: {},
+		resolve: {
+			operations: ['operationService', function (operationService) {
+				return operationService.findAllWithoutTimeEntry().then(function(ops){
+					var out = [], inx = 0;
+					ops.forEach(function (op) {
+						op.currentResources = _.filter(op.currentResources, {type:'DRIVER'});
+
+						if( op.currentResources.length > 0 ) {
+							out[inx] = op;
+						}
+						inx++;
+					});
+					return out;
+				});
+			}]
+		},
 		controller: require('./logbook-controller')
 	});
 }]);

5e384323ba783e21e8e112738634930592a685aa
Victor Escalante <vicx74ever@gmail.com>
2017-12-12 13:50:39 -0600

    Minor fixes in staff screen

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -225,62 +225,6 @@ function ($rootScope) {
 
 }])
 
-//CONTROLADOR DE PRUEBA PARA POSICIONAR DIV NUEVO app.js
-.controller('AddressCtrl',['$scope',
-function($scope){
-	$scope.addresses = [{type: '', country: '', fiscal:'',street1:'',street2:'',street3:'',city:'',state:'',zip:''}];
-  
-	$scope.addNewAddress = function() {
-	    var newItemNo = $scope.addresses.length+1;
-	    $scope.addresses.push({'id':'address'+newItemNo});
-	    $scope.itemAddress=newItemNo;
-	};
-
-	$scope.removeAddress = function(z) {
-	    //var lastItem = $scope.addresses.length-1;
-	    $scope.addresses.splice(z,1);
-	    //$scope.lastAddress=lastItem;
-	};
-
-}])
-
-//CONTROLADOR DE PRUEBA PARA POSICIONAR DIV NUEVO app.js
-.controller('PhoneCtrl',['$scope',
-function($scope){
-	$scope.phones = [{type: '', code: '', area:'',ext:'',sms:'',wps:''}];
-  
-	$scope.addNewPhone = function() {
-	    var newItemNo = $scope.phones.length+1;
-	    $scope.phones.push({'id':'phone'+newItemNo});
-	    $scope.itemPhone=newItemNo;
-	};
-
-	$scope.removePhone = function(z) {
-	    //var lastItem = $scope.phones.length-1;
-	    $scope.phones.splice(z,1);
-	    //$scope.lastPhone=lastItem;
-	};
-
-}])
-
-//CONTROLADOR DE PRUEBA PARA POSICIONAR DIV NUEVO app.js
-.controller('MailCtrl',['$scope',
-function($scope){
-	$scope.mails = [{type: '', mail: ''}];
-  
-	$scope.addNewMail = function() {
-	    var newItemNo = $scope.mails.length+1;
-	    $scope.mails.push({'id':'mail'+newItemNo});
-	    $scope.itemMail=newItemNo;
-	};
-
-	$scope.removeMail = function(z) {
-	    //var lastItem = $scope.mails.length-1;
-	    $scope.mails.splice(z,1);
-	    //$scope.lastMail=lastItem;
-	};
-
-}])
 
 .controller('asideMenu', ['$scope','$aside','security',
 function($scope, $aside, security) {

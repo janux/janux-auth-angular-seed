a6f973659e7705a3c69e26618944fbe2a4c534db
Ernesto Ortiz <ernesto.tito@gail.com>
2018-01-17 14:12:28 -0600

    Partial fix due to conflicting commits.

--- client/src/app/permission/perm-controller.js
+++ client/src/app/permission/perm-controller.js
@@ -1,95 +1,100 @@
 'use strict';
 
+var util = require('common/security/util');
+
 module.exports = [
-'$scope','authContextGroups','authContextService','$state','$modal', function(
- $scope , authContextGroups , authContextService , $state , $modal) {
+	'$scope','authContextGroups','authContextService','$state','$modal', function(
+		$scope , authContextGroups , authContextService , $state , $modal) {
 
-	$scope.authContextGroups = authContextGroups;
-	// console.log('authContextGroups', authContextGroups);
+		$scope.authContextGroups = authContextGroups;
+		// console.log('authContextGroups', authContextGroups);
 
-	//
-	// Delete authorization context
-	var deleteAuthContext = function (groupCode, name) {
-		return 	authContextService.deleteByName(groupCode, name)
-			.then(function () {
-				$state.go($state.current, {}, {reload: true});
-			});
-	};
-
-	var deleteContextCtrl = ['$scope','$modalInstance', 'data',
-					function($scope , $modalInstance, data) {
-		$scope.targetDescr = 'Authorization Context: '+data.name;
-
-		$scope.ok = function() {
-			deleteAuthContext(data.groupCode, data.name);
-			$modalInstance.close();
+		//
+		// Delete authorization context
+		var deleteAuthContext = function (groupCode, name) {
+			return 	authContextService.deleteByName(groupCode, name)
+				.then(function () {
+					$state.go($state.current, {}, {reload: true});
+				});
 		};
 
-		$scope.cancel = function() {
-			$modalInstance.dismiss();
-		};
-	}];
+		var deleteContextCtrl = ['$scope','$modalInstance', 'data',
+			function($scope , $modalInstance, data) {
+				$scope.targetDescr = 'Authorization Context: '+data.name;
 
-	$scope.openDeleteAuthContextDialog = function(groupCode, name){
-		$modal.open({
-			templateUrl: 'app/dialog-tpl/delete-dialog.html',
-			controller: deleteContextCtrl,
-			size: 'md',
-			resolve: {
-				data: function () {
-					return { groupCode:groupCode, name:name };
-				}
-			}
-		});
-	};
+				$scope.ok = function() {
+					deleteAuthContext(data.groupCode, data.name);
+					$modalInstance.close();
+				};
 
-	//
-	// Delete authorization context group
+				$scope.cancel = function() {
+					$modalInstance.dismiss();
+				};
+			}];
 
-	var deleteContextGroupCtrl = ['$scope','$modalInstance', 'group',
-		function($scope , $modalInstance, group) {
-			$scope.targetDescr = 'Authorization Context Group: '+group.name;
-
-			$scope.ok = function() {
-				return 	authContextService.removeGroup(group.code)
-					.then(function () {
-						$modalInstance.close();
-						$state.go($state.current, {}, {reload: true});
-					});
-			};
-
-			$scope.cancel = function() {
-				$modalInstance.dismiss();
-			};
-		}];
-
-	$scope.openDeleteAuthContextGroupDialog = function(group){
-
-		// Can't delete the group because it still contains linked authorization contexts
-		if(group.values.length) {
-			$modal.open({
-				templateUrl: 'app/dialog-tpl/info-dialog.html',
-				controller: ['$scope','$modalInstance',
-					function($scope , $modalInstance) {
-						$scope.message = 'You can not delete this group because it contains '+group.values.length+' linked authorization contexts';
-
-						$scope.ok = function() {
-							$modalInstance.close();
-						};
-					}],
-				size: 'md'
-			});
-		} else {
+		$scope.openDeleteAuthContextDialog = function(groupCode, name){
 			$modal.open({
 				templateUrl: 'app/dialog-tpl/delete-dialog.html',
-				controller: deleteContextGroupCtrl,
+				controller: deleteContextCtrl,
 				size: 'md',
 				resolve: {
-					group: function () {
-						return group;
+					data: function () {
+						return { groupCode:groupCode, name:name };
 					}
 				}
 			});
-		}
-	};
-}];
+		};
+
+		//
+		// Delete authorization context group
+
+		var deleteContextGroupCtrl = ['$scope','$modalInstance', 'group',
+			function($scope , $modalInstance, group) {
+				$scope.targetDescr = 'Authorization Context Group: '+group.name;
+
+				$scope.ok = function() {
+					return 	authContextService.removeGroup(group.code)
+						.then(function () {
+							$modalInstance.close();
+							$state.go($state.current, {}, {reload: true});
+						});
+				};
+
+				$scope.cancel = function() {
+					$modalInstance.dismiss();
+				};
+			}];
+
+		$scope.openDeleteAuthContextGroupDialog = function(group){
+
+			// Can't delete the group because it still contains linked authorization contexts
+			if(group.values.length) {
+				$modal.open({
+					templateUrl: 'app/dialog-tpl/info-dialog.html',
+					controller: ['$scope','$modalInstance',
+						function($scope , $modalInstance) {
+							$scope.message = 'You can not delete this group because it contains '+group.values.length+' linked authorization contexts';
+
+							$scope.ok = function() {
+								$modalInstance.close();
+							};
+						}],
+					size: 'md'
+				});
+			} else {
+				$modal.open({
+					templateUrl: 'app/dialog-tpl/delete-dialog.html',
+					controller: deleteContextGroupCtrl,
+					size: 'md',
+					resolve: {
+						group: function () {
+							return group;
+						}
+					}
+				});
+			}
+		};
+
+		// Convert object of authorization bits to array
+		$scope.authCBitsToArray = util.authCBitsToArray;
+	}];
--- client/src/app/role/role-create-controller.js
+++ client/src/app/role/role-create-controller.js
@@ -3,55 +3,59 @@
 var _ = require('lodash');
 var Role = require('janux-authorize').Role;
 var AuthorizationContext = require('janux-authorize').AuthorizationContext;
+var util = require('common/security/util');
 
 module.exports = [
-'$scope', 'authContextGroups','roleService','$state',
+	'$scope', 'authContextGroups','roleService','$state',
 	function($scope, authContextGroups, roleService, $state) {
 
-	$scope.authContextGroups = authContextGroups;
+		$scope.authContextGroups = authContextGroups;
 
-	$scope.cancel = function () {
-		window.history.back();
-	};
+		$scope.cancel = function () {
+			window.history.back();
+		};
 
-	$scope.role = {
-		'name': '',
-		'description': '',
-		'isAlmighty': false,
-		'enabled': true,
-		'authContexts': {}
-	};
+		$scope.role = {
+			'name': '',
+			'description': '',
+			'isAlmighty': false,
+			'enabled': true,
+			'authContexts': {}
+		};
 
-	$scope.createRole = function () {
+		$scope.createRole = function () {
 
-		// Instantiate the authorization contexts from which
-		// at least one permit has been granted for this role
+			// Instantiate the authorization contexts from which
+			// at least one permit has been granted for this role
 
-		// Extract authorization contexts
-		var authContexts = _.mapKeys(_.flatten(_.map(authContextGroups, 'values')),'name');
-		// console.log('authContexts', authContexts);
+			// Extract authorization contexts
+			var authContexts = _.mapKeys(_.flatten(_.map(authContextGroups, 'values')),'name');
+			// console.log('authContexts', authContexts);
 
-		// Create Role
-		var role = Role.createInstance($scope.role.name, $scope.role.description);
-		role.enabled = $scope.role.enabled;
-		role.isAlmighty = $scope.role.isAlmighty;
+			// Create Role
+			var role = Role.createInstance($scope.role.name, $scope.role.description);
+			role.enabled = $scope.role.enabled;
+			role.isAlmighty = $scope.role.isAlmighty;
 
-		for(var contextName in $scope.role.authContexts) {
+			for(var contextName in $scope.role.authContexts) {
 
-			// Create instance of authorization context
-			var givenContext = AuthorizationContext.fromJSON(authContexts[contextName]);
+				// Create instance of authorization context
+				var givenContext = AuthorizationContext.fromJSON(authContexts[contextName]);
 
-			// Get granted bits list
-			var grantedBits = [];
-			for (var grantedBit in _.values($scope.role.authContexts[contextName])[0]) {
-				grantedBits.push(grantedBit);
+				// Get granted bits list
+				var grantedBits = [];
+				for (var grantedBit in _.values($scope.role.authContexts[contextName])[0]) {
+					grantedBits.push(grantedBit);
+				}
+
+				role.grant(grantedBits, givenContext);
 			}
 
-			role.grant(grantedBits, givenContext);
-		}
+			roleService.insert(role.toJSON()).then(function(){
+				$state.go('permissions.roles');
+			});
+		};
 
-		roleService.insert(role.toJSON()).then(function(){
-			$state.go('permissions.roles');
-		});
-	};
-}];
+		// Convert object of authorization bits to array
+		$scope.authCBitsToArray = util.authCBitsToArray;
+	}];

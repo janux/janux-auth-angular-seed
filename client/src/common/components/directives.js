2c444d1d752ba5513824625735178f9e70dd18f2
Alfredo Alejandro Alvarez Ortiz <alejandro@januxmedia.com>
2018-02-02 20:17:13 -0600

    Adding common components module

---
+++ client/src/common/components/directives.js
@@ -0,0 +1,73 @@
+"use strict";
+
+var PhoneNumber = require('janux-people').PhoneNumber;
+var Email = require('janux-people').EmailAddress;
+var PostalAddress = require('janux-people').PostalAddress;
+
+var scopeDefinition = { "data":"=", "section":"@" };
+
+angular.module('commonComponents',[])
+
+.directive('personName', function() {
+	return{
+		scope: scopeDefinition,
+		restrict:'E',
+		templateUrl: 'common/components/templates/person-name.html'
+	};
+})
+.directive('personJob', function() {
+	return{
+		scope: scopeDefinition,
+		restrict:'E',
+		templateUrl: 'common/components/templates/person-job.html'
+	};
+})
+
+.directive('phones', function() {
+	return{
+		scope: scopeDefinition,
+		restrict:'E',
+		templateUrl: 'common/components/templates/phones.html',
+		controller: function ($scope) {
+			$scope.addNewPhone = function() {
+				$scope.data.setContactMethod('work', new PhoneNumber());
+			};
+
+			$scope.removePhone = function(z) {
+				$scope.data.contactMethods.phones.splice(z,1);
+			};
+		}
+	};
+})
+.directive('emails', function() {
+	return{
+		scope: scopeDefinition,
+		restrict:'E',
+		templateUrl: 'common/components/templates/emails.html',
+		controller: function ($scope) {
+			$scope.addNewMail = function() {
+				$scope.data.setContactMethod('work', new Email());
+			};
+
+			$scope.removeMail = function(z) {
+				$scope.data.contactMethods.emails.splice(z,1);
+			};
+		}
+	};
+})
+.directive('addresses', function() {
+	return{
+		scope: scopeDefinition,
+		restrict:'E',
+		templateUrl: 'common/components/templates/addresses.html',
+		controller: function ($scope) {
+			$scope.addNewAddress = function() {
+				$scope.data.setContactMethod('work', new PostalAddress());
+			};
+
+			$scope.removeAddress = function(z) {
+				$scope.data.contactMethods.addresses.splice(z,1);
+			};
+		}
+	};
+});

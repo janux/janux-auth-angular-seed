21a71059861c4b8ec5442f76816bfb163f56ace1
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-02-13 17:37:34 -0600

    New time entry guard report.
    Also when inserting or updating time entries. The resource type changes to make difference for night shit maintenance guards and good receipt guards.

--- server/route/time-entry-report.js
+++ server/route/time-entry-report.js
@@ -5,28 +5,49 @@
 
 'use strict';
 var
-	log                    = require('log4js').getLogger('time-entry-report'),
-	tokenHandler           = require("../src/auth/token-handler"),
-	timeEntryReportService = require("../src/api/index").TimeEntryReportService;
+	log                         = require('log4js').getLogger('time-entry-report'),
+	tokenHandler                = require("../src/auth/token-handler"),
+	timeEntryReportService      = require("../src/api/index").TimeEntryReportService,
+	timeEntryReportGuardService = require("../src/api/index").TimeEntryReportGuardService;
 
 module.exports = function (app) {
 	app.post('/time-entry-report', tokenHandler.authenticate, tokenHandler.handleInvalidTokenAuth, function (req, res) {
 
 		try {
-			
 			const ids = req.body.ids;
 			const timeZone = req.body.timeZone;
 			timeEntryReportService.generateReport(ids, timeZone)
 				.then(function (result) {
 					log.debug("Report at %j ", result);
 					res.download(result, function (err) {
-						if(err) {
+						if (err) {
 							log.error("Error downloading file " + err);
 						}
-					})
+					});
 				});
 		} catch (err) {
 			log.error('Error generating time entry report file: ' + err);
 		}
 	});
+
+
+	app.post('/time-entry-report-guard', tokenHandler.authenticate, tokenHandler.handleInvalidTokenAuth, function (req, res) {
+
+		try {
+			const ids = req.body.ids;
+			const timeZone = req.body.timeZone;
+			timeEntryReportGuardService.generateReport(ids, timeZone)
+				.then(function (result) {
+					log.debug("Report at %j ", result);
+					res.download(result, function (err) {
+						if (err) {
+							log.error("Error downloading file " + err);
+						}
+					});
+				});
+		} catch (err) {
+			log.error('Error generating time entry report file: ' + err);
+		}
+	});
+
 };

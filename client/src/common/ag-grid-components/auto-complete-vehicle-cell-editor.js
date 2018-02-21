9e758c96ff314a1d328e926b850c89c468d52e32
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-02-21 17:58:01 -0600

    edited md-item-template

--- client/src/common/ag-grid-components/auto-complete-vehicle-cell-editor.js
+++ client/src/common/ag-grid-components/auto-complete-vehicle-cell-editor.js
@@ -7,19 +7,28 @@ function AutoCompleteVehicle() {
 // gets called once before the renderer is used
 AutoCompleteVehicle.prototype.init = function (params) {
 	// Assign value to row scope
-	this.model = 'autocomplete' + params.column.colId;
-	// Access to complete staff object
-	params.$scope[this.model] = params.$scope.data[params.column.colId];
-	this.rowScope = params.$scope;
+	// this.model = 'autocomplete' + params.column.colId;
+	// // Access to complete staff object
+	// params.$scope[this.model] = params.$scope.data[params.column.colId];
+	// this.rowScope = params.$scope;
+//
+	// // Create angular material autocomplete
+	// this.autocomplete = document.createElement('ag-grid-vehicle-autocomplete');
+	// this.autocomplete.setAttribute('selected-value-model', this.model);
 
-	// Create angular material autocomplete
-	this.autocomplete = document.createElement('ag-grid-vehicle-autocomplete');
-	this.autocomplete.setAttribute('selected-value-model', this.model);
+
+	this.model = 'vehicleAutoComplete'+params.column.colId;
+	params.$scope[this.model] = params.value;
+	this.eInput = document.createElement('input');
+	this.eInput.value = params.value;
+	this.eInput.setAttribute('ag-grid-vehicle-autocomplete', '');
+	this.eInput.setAttribute('ng-model', this.model);
+
 };
 
 // gets called once when grid ready to insert the element
 AutoCompleteVehicle.prototype.getGui = function () {
-	return this.autocomplete;
+	return this.eInput;
 };
 
 // focus and select can be done after the gui is attached
@@ -29,7 +38,7 @@ AutoCompleteVehicle.prototype.afterGuiAttached = function () {
 
 // returns the new value after editing
 AutoCompleteVehicle.prototype.getValue = function () {
-	return this.rowScope[this.model];
+	return this.eInput.value;
 };
 
 // any cleanup we need to be done here

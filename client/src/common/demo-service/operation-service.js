c5cd75d03ae11351eb802e4b4e31ab18c3119435
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-02-23 19:50:28 -0600

    Fis a bug for personnel selection in the ag-grid.
    Allow in the guards time entries allow to insert time entries with no person when capturing a  "closed store record."

--- client/src/common/demo-service/operation-service.js
+++ client/src/common/demo-service/operation-service.js
@@ -54,19 +54,19 @@ module.exports =
 			return result;
 		}
 
-	function calculateDuration(begin, end) {
-		var duration = '0:0';
+		function calculateDuration(begin, end) {
+			var duration = '0:0';
 
-		if (_.isNil(end) === false) {
-			end = moment(end);
-			var durationMoment = moment.duration(end.diff(begin));
-			var daysToHours = (durationMoment.get("days") > 0) ? durationMoment.get("days") * 24 : 0;
-			duration = (durationMoment.get("hours") + daysToHours) + ":" + ('00' + durationMoment.get("minutes")).slice(-2);
+			if (_.isNil(end) === false) {
+				end = moment(end);
+				var durationMoment = moment.duration(end.diff(begin));
+				var daysToHours = (durationMoment.get("days") > 0) ? durationMoment.get("days") * 24 : 0;
+				duration = (durationMoment.get("hours") + daysToHours) + ":" + ('00' + durationMoment.get("minutes")).slice(-2);
+			}
+			return duration;
 		}
-		return duration;
-	}
 
-	var service = {
+		var service = {
 
 			findByDateBetweenWithTimeEntriesAndType: function (initDate, endDate, type) {
 				return $http.jsonrpc(
@@ -243,11 +243,11 @@ module.exports =
 
 
 						return {
-							driversAssignedToOperations : driversAssignedToOperations,
-							vehiclesAssignedToOperations: vehiclesAssignedToOperations,
-							drivers                     : allDriversAvailableForSelection,
-							vehicles                    : allVehiclesAvailableForSelection,
-							operations                  : operations
+							driversAssignedToOperations      : driversAssignedToOperations,
+							vehiclesAssignedToOperations     : vehiclesAssignedToOperations,
+							allPersonnelAvailableForSelection: allDriversAvailableForSelection,
+							vehicles                         : allVehiclesAvailableForSelection,
+							operations                       : operations
 						};
 					});
 				});
@@ -262,19 +262,19 @@ module.exports =
 						var timeEntry = operation.schedule[j];
 						var begin = moment(timeEntry.begin);
 
-					var duration = calculateDuration(timeEntry.begin, timeEntry.end);
-					var end = '', endOnlyHour = '';
+						var duration = calculateDuration(timeEntry.begin, timeEntry.end);
+						var end = '', endOnlyHour = '';
 
-					if (_.isNil(timeEntry.end) === false) {
-						end = moment(timeEntry.end);
-						endOnlyHour = end.format(formatStringOnlyHour);
-						end = end.format(dateTimeFormatString);
-					}
-					// Temporary solution to mark records without absence
-					if( operation.type === 'DRIVER') {
-						var absence = (!_.isNil(timeEntry.resources[0].absence) && timeEntry.resources[0].absence !== '') ?
-							timeEntry.resources[0].absence : 'SF';
-					}
+						if (_.isNil(timeEntry.end) === false) {
+							end = moment(timeEntry.end);
+							endOnlyHour = end.format(formatStringOnlyHour);
+							end = end.format(dateTimeFormatString);
+						}
+						// Temporary solution to mark records without absence
+						if (operation.type === 'DRIVER') {
+							var absence = (!_.isNil(timeEntry.resources[0].absence) && timeEntry.resources[0].absence !== '') ?
+								timeEntry.resources[0].absence : 'SF';
+						}
 
 						// Temporary solution for empty extras.
 						if (operation.type === 'GUARD' && (_.isNil(timeEntry.extras) || timeEntry.extras === '')) {
@@ -313,42 +313,42 @@ module.exports =
 				return result;
 			},
 
-		// Map operations properties in order to create a list with ag-grid or similar
-		mapOperations: function (operations) {
+			// Map operations properties in order to create a list with ag-grid or similar
+			mapOperations: function (operations) {
 
-			return _.map(operations, function (operation) {
+				return _.map(operations, function (operation) {
 
-				const duration = (!_.isNil(operation.start))?moment(operation.start).diff(moment(operation.end)):'';
-				const start = (!_.isNil(operation.start))?moment(operation.start).format('YYYY-MM-DD'):'';
-				const end = (!_.isNil(operation.end))?moment(operation.end).format('YYYY-MM-DD'):'';
+					const duration = (!_.isNil(operation.start)) ? moment(operation.start).diff(moment(operation.end)) : '';
+					const start = (!_.isNil(operation.start)) ? moment(operation.start).format('YYYY-MM-DD') : '';
+					const end = (!_.isNil(operation.end)) ? moment(operation.end).format('YYYY-MM-DD') : '';
 
-				var assigned = '';
-				if(operation.currentResources.length > 0 ){
-					assigned = operation.currentResources[0].resource;
-					assigned = assigned.name.last+' '+assigned.name.first;
-				}
+					var assigned = '';
+					if (operation.currentResources.length > 0) {
+						assigned = operation.currentResources[0].resource;
+						assigned = assigned.name.last + ' ' + assigned.name.first;
+					}
 
-				return {
-					id: operation.id,
-					type: operation.type,
-					name: operation.name,
-					client: operation.client.name,
-					assigned: assigned,
-					duration: duration,
-					start: start,
-					end: end
-				}
-			});
-		},
+					return {
+						id      : operation.id,
+						type    : operation.type,
+						name    : operation.name,
+						client  : operation.client.name,
+						assigned: assigned,
+						duration: duration,
+						start   : start,
+						end     : end
+					}
+				});
+			},
 
-		fromJSON: function (object) {
-			return fromJSON(object);
-		},
+			fromJSON: function (object) {
+				return fromJSON(object);
+			},
 
-		toJSON: function (object) {
-			return toJSON(object);
-		}
+			toJSON: function (object) {
+				return toJSON(object);
+			}
 
-	};
-	return service;
-}];
+		};
+		return service;
+	}];

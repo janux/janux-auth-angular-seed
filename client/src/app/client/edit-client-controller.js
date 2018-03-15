5949389f169a0eeb5148b108c0cfd9c005a8152f
Alfredo Alejandro Alvarez Ortiz <alejandro@januxmedia.com>
2018-03-14 19:17:07 -0600

    Fine-tune breadcrumb in client edit page

--- client/src/app/client/edit-client-controller.js
+++ client/src/app/client/edit-client-controller.js
@@ -1,18 +1,35 @@
 'use strict';
+
+var _ = require('lodash');
+
 var PhoneNumber = require('janux-people').PhoneNumber;
 var Email = require('janux-people').EmailAddress;
 var PostalAddress = require('janux-people').PostalAddress;
 
-
 module.exports = [
-'$scope','partyService','$state','client','contacts', function(
- $scope , partyService , $state , client , contacts) {
+'$scope','partyService','$state','client','contacts','$modal','$filter', function(
+ $scope , partyService , $state , client , contacts , $modal , $filter ) {
 
 	console.log('client being edited', client, 'contacts', contacts);
 	$scope.currentNavItem = 'client';
 
 	$scope.client = client;
 	$scope.contacts = contacts;
+
+	var infoDialog = function (translateKey) {
+		$modal.open({
+			templateUrl: 'app/dialog-tpl/info-dialog.html',
+			controller : ['$scope', '$modalInstance',
+				function ($scope, $modalInstance) {
+					$scope.message = $filter('translate')(translateKey);
+
+					$scope.ok = function () {
+						$modalInstance.close();
+					};
+				}],
+			size       : 'md'
+		});
+	};
 
 	$scope.addNewAddress = function() {
 	    $scope.client.setContactMethod('work', new PostalAddress());
@@ -47,5 +64,53 @@ module.exports = [
 		
 	$scope.cancel = function () {
 		window.history.back();
+	};
+
+	// Disable selected client's contacts
+	$scope.disableSelectedContacts= function () {
+
+		var selectedContacts = _.chain($scope.contacts)
+			.filter({selected:true})
+			.map('id')
+			.value();
+
+		if (selectedContacts.length > 0) {
+			$modal.open({
+				templateUrl: 'app/dialog-tpl/confirm-dialog.html',
+				controller : ['$scope', '$modalInstance',
+					function ($scope, $modalInstance) {
+						$scope.message = $filter('translate')('client.dialogs.confirmDeletion');
+
+						$scope.ok = function () {
+							infoDialog('To be implemented!');
+
+							// Disable contacts
+							// var promises = _.map(selectedContacts, function(contactId){
+							//
+							// 	// TODO: Implement method to disable contacts
+							// 	return partyService.remove(contactId);
+							// });
+
+							// $q.all(promises).then(function(){
+							// 	$mdToast.show(
+							// 		$mdToast.simple()
+							// 			.textContent($filter('translate')('client.dialogs.successDeletedContacts'))
+							// 			.position( 'top right' )
+							// 			.hideDelay(3000)
+							// 	);
+							// });
+
+							$modalInstance.close();
+						};
+
+						$scope.cancel = function () {
+							$modalInstance.close();
+						};
+					}],
+				size       : 'md'
+			});
+		} else {
+			infoDialog('client.dialogs.noRowSelectedError');
+		}
 	};
 }];

651c12607ec3b3b810ba68b17bff45f040e38a42
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-03-16 19:43:56 -0600

    Added metadata to the time entries.

--- server/src/api/daos.js
+++ server/src/api/daos.js
@@ -1,77 +1,80 @@
 'use strict';
 
 var config               = require('config').serverAppContext,
-	DataSourceHandler    = require('janux-persist').DataSourceHandler,
-	DaoFactory           = require('janux-persist').DaoFactory,
-	EntityPropertiesImpl = require('janux-persist').EntityPropertiesImpl,
-	DaoSettings          = require('janux-persist').DaoSettings,
-	dbEngine             = config.db.dbEngine,
-	pathLoki             = config.db.lokiJsDBPath,
-	pathMongo            = config.db.mongoConnUrl;
+    DataSourceHandler    = require('janux-persist').DataSourceHandler,
+    DaoFactory           = require('janux-persist').DaoFactory,
+    EntityPropertiesImpl = require('janux-persist').EntityPropertiesImpl,
+    DaoSettings          = require('janux-persist').DaoSettings,
+    dbEngine             = config.db.dbEngine,
+    pathLoki             = config.db.lokiJsDBPath,
+    pathMongo            = config.db.mongoConnUrl;
 
 
 const PartyDaoMongooseImpl       = require('janux-persist').PartyDaoMongooseImpl,
-	  AccountDaoMongooseImpl     = require('janux-persist').AccountDaoMongooseImpl,
-	  AuthContextDaoMongooseImpl = require('janux-persist').AuthContextDaoMongooseImpl,
-	  RoleDaoMongooseImpl        = require('janux-persist').RoleDaoMongooseImpl,
-	  GroupDao                   = require('janux-persist').GroupDao,
-	  GroupContentDao            = require('janux-persist').GroupContentDao,
-	  GroupAttributeValueDao     = require('janux-persist').GroupAttributeValueDao,
-	  PartyDaoLokiJsImpl         = require('janux-persist').PartyDaoLokiJsImpl,
-	  AccountDaoLokiJsImpl       = require('janux-persist').AccountDaoLokiJsImpl,
-	  AuthContextDaoLokiJsImpl   = require('janux-persist').AuthContextDaoLokiJsImpl,
-	  RoleDaoLokiJsImpl          = require('janux-persist').RoleDaoLokiJsImpl;
+      AccountDaoMongooseImpl     = require('janux-persist').AccountDaoMongooseImpl,
+      AuthContextDaoMongooseImpl = require('janux-persist').AuthContextDaoMongooseImpl,
+      RoleDaoMongooseImpl        = require('janux-persist').RoleDaoMongooseImpl,
+      GroupDao                   = require('janux-persist').GroupDao,
+      GroupContentDao            = require('janux-persist').GroupContentDao,
+      GroupAttributeValueDao     = require('janux-persist').GroupAttributeValueDao,
+      PartyDaoLokiJsImpl         = require('janux-persist').PartyDaoLokiJsImpl,
+      AccountDaoLokiJsImpl       = require('janux-persist').AccountDaoLokiJsImpl,
+      AuthContextDaoLokiJsImpl   = require('janux-persist').AuthContextDaoLokiJsImpl,
+      RoleDaoLokiJsImpl          = require('janux-persist').RoleDaoLokiJsImpl;
 
 
 const PartyMongooseSchema               = require('janux-persist').PartyMongooseSchema,
-	  AccountMongooseDbSchema           = require('janux-persist').AccountMongooseDbSchema,
-	  AuthContextMongooseDbSchema       = require('janux-persist').AuthContextMongooseDbSchema,
-	  RoleMongooseDbSchema              = require('janux-persist').RoleMongooseDbSchema,
-	  GroupMongooseSchema               = require('janux-persist').GroupMongooseSchema,
-	  GroupContentMongooseSchema        = require('janux-persist').GroupContentMongooseSchema,
-	  GroupAttributeValueMongooseSchema = require('janux-persist').GroupAttributeValueMongooseSchema;
+      AccountMongooseDbSchema           = require('janux-persist').AccountMongooseDbSchema,
+      AuthContextMongooseDbSchema       = require('janux-persist').AuthContextMongooseDbSchema,
+      RoleMongooseDbSchema              = require('janux-persist').RoleMongooseDbSchema,
+      GroupMongooseSchema               = require('janux-persist').GroupMongooseSchema,
+      GroupContentMongooseSchema        = require('janux-persist').GroupContentMongooseSchema,
+      GroupAttributeValueMongooseSchema = require('janux-persist').GroupAttributeValueMongooseSchema;
 
 const PARTY_DEFAULT_COLLECTION_NAME            = 'contact',
-	  ACCOUNT_DEFAULT_COLLECTION_NAME          = 'account',
-	  AUTHCONTEXT_DEFAULT_COLLECTION_NAME      = 'authcontext',
-	  ROLE_DEFAULT_COLLECTION_NAME             = 'role',
-	  GROUP_DEFAULT_COLLECTION_NAME            = 'group',
-	  GROUP_CONTENT_DEFAULT_COLLECTION_NAME    = 'groupContent',
-	  GROUP_ATTRIBUTES_DEFAULT_COLLECTION_NAME = "groupAttribute";
+      ACCOUNT_DEFAULT_COLLECTION_NAME          = 'account',
+      AUTHCONTEXT_DEFAULT_COLLECTION_NAME      = 'authcontext',
+      ROLE_DEFAULT_COLLECTION_NAME             = 'role',
+      GROUP_DEFAULT_COLLECTION_NAME            = 'group',
+      GROUP_CONTENT_DEFAULT_COLLECTION_NAME    = 'groupContent',
+      GROUP_ATTRIBUTES_DEFAULT_COLLECTION_NAME = "groupAttribute";
 
 
 // Begin Glarus services DAOs.
 
 const VehicleDao            = require("glarus-services").VehicleDao,
-	  TimeEntryPrincipalDao = require("glarus-services").TimeEntryPrincipalDao,
-	  OperationDao          = require("glarus-services").OperationDao,
-	  OperationPrincipalDao = require("glarus-services").OperationPrincipalDao,
-	  TimeEntryDao          = require("glarus-services").TimeEntryDao,
-	  ResourceDao           = require("glarus-services").ResourceDao,
-	  CurrentResourceDao    = require("glarus-services").CurrentResourceDao,
-	  TimeEntryAttributeDao = require("glarus-services").TimeEntryAttributeDao,
-	  TimeEntryResourceDao  = require("glarus-services").TimeEntryResourceDao;
+      TimeEntryPrincipalDao = require("glarus-services").TimeEntryPrincipalDao,
+      OperationDao          = require("glarus-services").OperationDao,
+      OperationAttributeDao = require("glarus-services").OperationAttributeDao,
+      OperationPrincipalDao = require("glarus-services").OperationPrincipalDao,
+      TimeEntryDao          = require("glarus-services").TimeEntryDao,
+      ResourceDao           = require("glarus-services").ResourceDao,
+      CurrentResourceDao    = require("glarus-services").CurrentResourceDao,
+      TimeEntryAttributeDao = require("glarus-services").TimeEntryAttributeDao,
+      TimeEntryResourceDao  = require("glarus-services").TimeEntryResourceDao;
 
 
 const VehicleMongooseSchema            = require("glarus-services").VehicleMongooseSchema,
-	  TimeEntryPrincipalMongooseSchema = require("glarus-services").TimeEntryPrincipalMongooseSchema,
-	  TimeEntryMongooseSchema          = require("glarus-services").TimeEntryMongooseSchema,
-	  OperationMongooseSchema          = require("glarus-services").OperationMongooseSchema,
-	  OperationPrincipalMongooseSchema = require("glarus-services").OperationPrincipalMongooseSchema,
-	  ResourceMongooseSchema           = require("glarus-services").ResourceMongooseSchema,
-	  CurrentResourceMongooseSchema    = require("glarus-services").CurrentResourceMongooseSchema,
-	  TimeEntryAttributeMongooseSchema = require("glarus-services").TimeEntryAttributeMongooseSchema,
-	  TimeEntryResourceMongooseSchema  = require("glarus-services").TimeEntryResourceMongooseSchema;
+      TimeEntryPrincipalMongooseSchema = require("glarus-services").TimeEntryPrincipalMongooseSchema,
+      TimeEntryMongooseSchema          = require("glarus-services").TimeEntryMongooseSchema,
+      OperationMongooseSchema          = require("glarus-services").OperationMongooseSchema,
+      OperationAttributeMongooseSchema = require("glarus-services").OperationAttributeMongooseSchema,
+      OperationPrincipalMongooseSchema = require("glarus-services").OperationPrincipalMongooseSchema,
+      ResourceMongooseSchema           = require("glarus-services").ResourceMongooseSchema,
+      CurrentResourceMongooseSchema    = require("glarus-services").CurrentResourceMongooseSchema,
+      TimeEntryAttributeMongooseSchema = require("glarus-services").TimeEntryAttributeMongooseSchema,
+      TimeEntryResourceMongooseSchema  = require("glarus-services").TimeEntryResourceMongooseSchema;
 
 const VEHICLE_COLLECTION              = "vehicle",
-	  TIME_ENTRY_COLLECTION           = "timeEntry",
-	  TIME_ENTRY_PRINCIPAL_COLLECTION = "timeEntryPrincipal",
-	  OPERATION_COLLECTION            = "operation",
-	  OPERATION_PRINCIPAL_COLLECTION  = "operationPrincipal",
-	  RESOURCE_COLLECTION             = "resource",
-	  TIME_ENTRY_ATTRIBUTE_COLLECTION = "timeEntryAttribute",
-	  CURRENT_RESOURCE_COLLECTION     = "currentResource",
-	  TIME_ENTRY_RESOURCE_COLLECTION  = "timeEntryResource";
+      TIME_ENTRY_COLLECTION           = "timeEntry",
+      TIME_ENTRY_PRINCIPAL_COLLECTION = "timeEntryPrincipal",
+      OPERATION_COLLECTION            = "operation",
+      OPERATION_ATTRIBUTE_COLLECTION  = "operationAttribute",
+      OPERATION_PRINCIPAL_COLLECTION  = "operationPrincipal",
+      RESOURCE_COLLECTION             = "resource",
+      TIME_ENTRY_ATTRIBUTE_COLLECTION = "timeEntryAttribute",
+      CURRENT_RESOURCE_COLLECTION     = "currentResource",
+      TIME_ENTRY_RESOURCE_COLLECTION  = "timeEntryResource";
 
 // End Glarus services DAOs.
 
@@ -91,6 +94,7 @@ if (dbEngine === DataSourceHandler.MONGOOSE) {
 		vehicleDao           : DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, VEHICLE_COLLECTION, EntityPropertiesImpl.createDefaultProperties(), VehicleMongooseSchema), VehicleDao),
 		timeEntryPrincipalDao: DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, TIME_ENTRY_PRINCIPAL_COLLECTION, EntityPropertiesImpl.createDefaultProperties(), TimeEntryPrincipalMongooseSchema), TimeEntryPrincipalDao),
 		operationDao         : DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, OPERATION_COLLECTION, EntityPropertiesImpl.createDefaultProperties(), OperationMongooseSchema), OperationDao),
+		operationAttributeDao: DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, OPERATION_ATTRIBUTE_COLLECTION, EntityPropertiesImpl.createDefaultProperties(), OperationAttributeMongooseSchema), OperationAttributeDao),
 		operationPrincipalDao: DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, OPERATION_PRINCIPAL_COLLECTION, EntityPropertiesImpl.createDefaultProperties(), OperationPrincipalMongooseSchema), OperationPrincipalDao),
 		timeEntryDao         : DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, TIME_ENTRY_COLLECTION, EntityPropertiesImpl.createDefaultProperties(), TimeEntryMongooseSchema), TimeEntryDao),
 		resourceDao          : DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, RESOURCE_COLLECTION, EntityPropertiesImpl.createDefaultProperties(), ResourceMongooseSchema), ResourceDao),
@@ -113,6 +117,7 @@ if (dbEngine === DataSourceHandler.MONGOOSE) {
 		vehicleDao           : DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, VEHICLE_COLLECTION, EntityPropertiesImpl.createDefaultProperties()), VehicleDao),
 		timeEntryPrincipalDao: DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, TIME_ENTRY_PRINCIPAL_COLLECTION, EntityPropertiesImpl.createDefaultProperties()), TimeEntryPrincipalDao),
 		operationDao         : DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, OPERATION_COLLECTION, EntityPropertiesImpl.createDefaultProperties()), OperationDao),
+		operationAttributeDao: DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, OPERATION_ATTRIBUTE_COLLECTION, EntityPropertiesImpl.createDefaultProperties()), OperationAttributeDao),
 		operationPrincipalDao: DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, OPERATION_PRINCIPAL_COLLECTION, EntityPropertiesImpl.createDefaultProperties()), OperationPrincipalDao),
 		timeEntryDao         : DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, TIME_ENTRY_COLLECTION, EntityPropertiesImpl.createDefaultProperties()), TimeEntryDao),
 		resourceDao          : DaoFactory.subscribeDao(new DaoSettings(dbEngine, pathMongo, RESOURCE_COLLECTION, EntityPropertiesImpl.createDefaultProperties()), ResourceDao),

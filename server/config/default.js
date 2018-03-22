d0fa6383adf896dac8a974f611dff704c8ef6965
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-03-22 17:34:03 -0600

    Adapted demo to the new partyService.

--- server/config/default.js
+++ server/config/default.js
@@ -6,35 +6,39 @@ module.exports = {
 
 	serverAppContext: {
 		server: {
-			port: 9000,
-			staticUrl: '',
+			port      : 9000,
+			staticUrl : '',
 			distFolder: path.join('..', 'client', 'dist'),
 			livereload: false,
-			secret: 'jules cyril value main why latex snowden obama'
+			secret    : 'jules cyril value main why latex snowden obama'
 		},
-		//Daos to be used by the services.
-		accountDao: 'accountDao',
-		partyDao: 'partyDao',
-		authContextDao: 'authContextDao',
-		roleDao: 'roleDao',
-		groupContentDao: 'groupContentDao',
-		groupDao: 'groupDao',
-		groupAttributeValueDao: 'groupAttributeValueDao',
-		// Glarus services daos.
-		vehicleDao: 'vehicleDao',
-		timeEntryPrincipalDao: 'timeEntryPrincipalDao',
-		operationDao: 'operationDao',
-		operationAttributeDao: 'operationAttributeDao',
-		operationPrincipalDao: 'operationPrincipalDao',
-		timeEntryDao: 'timeEntryDao',
-		resourceDao: 'resourceDao',
-		currentResourceDao: 'currentResourceDao',
-		timeEntryAttributeDao: 'timeEntryAttributeDao',
-		timeEntryResourceDao: 'timeEntryResourceDao',
-		// End glarus services daos.
+
+		dao: {
+			//Daos to be used by the services.
+			accountDao            : 'accountDao',
+			partyDao              : 'partyDao',
+			authContextDao        : 'authContextDao',
+			roleDao               : 'roleDao',
+			groupContentDao       : 'groupContentDao',
+			groupDao              : 'groupDao',
+			groupAttributeValueDao: 'groupAttributeValueDao',
+			staffDao              : 'staffDao',
+			// Glarus services daos.
+			vehicleDao            : 'vehicleDao',
+			timeEntryPrincipalDao : 'timeEntryPrincipalDao',
+			operationDao          : 'operationDao',
+			operationAttributeDao : 'operationAttributeDao',
+			operationPrincipalDao : 'operationPrincipalDao',
+			timeEntryDao          : 'timeEntryDao',
+			resourceDao           : 'resourceDao',
+			currentResourceDao    : 'currentResourceDao',
+			timeEntryAttributeDao : 'timeEntryAttributeDao',
+			timeEntryResourceDao  : 'timeEntryResourceDao',
+			// End glarus services daos.
+		},
 
 		log4js: {
-			config: require('./log4js.js'),
+			config        : require('./log4js.js'),
 			globalLogLevel: 'INFO'
 		},
 
@@ -44,7 +48,7 @@ module.exports = {
 			//Because this setting is not used for the daos. Just make use
 			//the db you are going to use for user generation is the same
 			//for the daos.
-			dbEngine: "mongoose",
+			dbEngine    : "mongoose",
 			//If mongodb is chosen for user generation and daos, you must define the connection url.
 			mongoConnUrl: "mongodb://localhost/opsGlarus",
 			//If lokijs is defined for user generation and daos, you must define the path of the file database.

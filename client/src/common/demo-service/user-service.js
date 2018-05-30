baba11a0a45e10243196eef84c4cd8f3a2a9987d
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-05-30 10:29:01 -0500

    Fix a bug with the method findCompanyInfo.

--- client/src/common/demo-service/user-service.js
+++ client/src/common/demo-service/user-service.js
@@ -6,9 +6,6 @@ module.exports =
 ['$q', '$http',
 function( $q ,  $http){
 
-	// TODO. Improve a better way to cache this information.
-	var cachedCompanyInfo;
-
 	var service = {
 
 
@@ -63,24 +60,16 @@ function( $q ,  $http){
 		},
 
 		findCompanyInfo: function(username){
-			if (_.isNil(cachedCompanyInfo)) {
-				return $http.jsonrpc(
-					'/rpc/2.0/users',
-					'findCompanyInfo',
-					[ username ]
-				).then(function(resp) {
-					// TODO. For some reason, I can't import PartyService, maybe due to some circular dependency.
-					// return PartyService.fromJSON(resp.data.result);
-					if (!_.isNil(resp.data.result)) {
-						cachedCompanyInfo = resp.data.result;
-					}else{
-						cachedCompanyInfo = undefined;
-					}
-					return resp.data.result;
-				});
-			}else{
-				return $q.resolve(cachedCompanyInfo);
-			}
+			return $http.jsonrpc(
+				'/rpc/2.0/users',
+				'findCompanyInfo',
+				[ username ]
+			).then(function(resp) {
+				// TODO. For some reason, I can't import PartyService, maybe due to some circular dependency.
+				// return PartyService.fromJSON(resp.data.result);
+
+				return resp.data.result;
+			});
 		},
 
 		// Add new user

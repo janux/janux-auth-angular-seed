b93f92b6c5f9d28d2800931bc5c831f04e4cdc4b
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-06-05 10:26:56 -0500

    Fix a style definition that causes an error in IE.

--- client/src/common/ag-grid-components/checkBoxRowSelection.js
+++ client/src/common/ag-grid-components/checkBoxRowSelection.js
@@ -7,7 +7,7 @@ function CheckBoxRowSelection() {
 CheckBoxRowSelection.prototype.init = function (params) {
 	this.eGui = document.createElement('div');
 	this.eGui.align='center';
-	this.eGui.style='padding:0px 5px 5px 14px;';
+	this.eGui.style.cssText='padding:0px 5px 5px 14px;';
 	
 
 	var checkbox = document.createElement('md-checkbox');

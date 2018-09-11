a0bf7a87f432161b7bd28133ce95d2122624b562
Alfredo Alejandro Alvarez Ortiz <alejandro@januxmedia.com>
2018-09-11 17:09:25 -0500

    Move user action methods logic to janux-persist

--- server/src/api/user/user-inv-service.js
+++ server/src/api/user/user-inv-service.js
@@ -7,40 +7,41 @@ var log4js           = require('log4js'),
 	UserInvServiceProd  = require('./user-inv-service.ext.prod'),
 	bluebird         = require('bluebird'),
 	log = log4js.getLogger('UserInvService'),
-	JanuxEnvironmentService = require('janux-persist').EnvironmentService;
+	JanuxEnvironmentService = require('janux-persist').EnvironmentService,
+	config				  = require('config').serverAppContext;
 
 // variable to hold the singleton instance, if used in that manner
 var userInvServiceInstance = undefined;
 
-var userServicePersistence = undefined;
-var commServicePersistence = undefined;
-var partyServicePersistence = undefined;
+// var userServicePersistence = undefined;
+// var commServicePersistence = undefined;
+// var partyServicePersistence = undefined;
 var userInvServicePersistence = undefined;
 
 //
 // Example of user service
 //
 
-var createInstance = function (userServiceReference, partyServiceReference, commServiceReference, userInvitationServiceReference) {
+var createInstance = function (userInvitationServiceReference) {
 
-	userServicePersistence = userServiceReference;
-	partyServicePersistence = partyServiceReference;
-	commServicePersistence = commServiceReference;
+	// userServicePersistence = userServiceReference;
+	// partyServicePersistence = partyServiceReference;
+	// commServicePersistence = commServiceReference;
 	userInvServicePersistence = userInvitationServiceReference;
 
-	// TODO: Move method logic to janux-persist
 	// var env = JanuxEnvironmentService.getEnvironmentInfo().environment;
-	var env = 'production'; // Needed because we are testing production methods en ops-staging
-	var UserInvServiceExt = (env === 'development') ? UserInvServiceDev : UserInvServiceProd;
-	var that;
+	// var env = 'production'; // Needed because we are testing production methods en ops-staging
+	// var UserInvServiceExt = (env === 'development') ? UserInvServiceDev : UserInvServiceProd;
+	// var that;
 
 	// Constructor
-	function UserInvService(userService, partyService, commService, userInvService) {
-		UserInvServiceExt.call(this, userService, partyService, commService, userInvService);
-		that = this;
+	function UserInvService() {
+		// UserInvServiceExt.call(this, userService, partyService, commService, userInvService);
+		// that = this;
 	}
 
-	UserInvService.prototype = Object.create(UserInvServiceExt.prototype);
+	// UserInvService.prototype = Object.create(UserInvServiceExt.prototype);
+	UserInvService.prototype = Object.create(null);
 	UserInvService.prototype.constructor = UserInvService;
 
 	//
@@ -68,10 +69,29 @@ var createInstance = function (userServiceReference, partyServiceReference, comm
 	};
 
 	UserInvService.prototype.recoverPassword = function (accountId, contactId, selectedEmail, callback) {
-		return UserInvServiceExt.prototype.recoverPassword.call(that, accountId, contactId, selectedEmail).asCallback(callback);
+		// return UserInvServiceExt.prototype.recoverPassword.call(that, accountId, contactId, selectedEmail).asCallback(callback);
+		var configObj = {
+			selectedEmail: selectedEmail,
+			msgSubject: 'Recuperación de contraseña para el sistema Glarus',
+			templateUrl: '../server/src/templates/recover-email.pug',
+			hostname: config.server.hostname
+		};
+		return userInvServicePersistence.recoverPassword(accountId, contactId, configObj).asCallback(callback);
 	};
 
-	return new UserInvService(userServicePersistence, partyServicePersistence, commServicePersistence, userInvServicePersistence);
+	UserInvService.prototype.inviteToCreateAccount = function (contactId, selectedEmail, assignedRoles, callback) {
+		// return UserInvServiceExt.prototype.recoverPassword.call(that, accountId, contactId, selectedEmail).asCallback(callback);
+		var  configObj = {
+			selectedEmail: selectedEmail,
+			msgSubject: 'Invitación al sistema de glarus',
+			templateUrl: '../server/src/templates/user-invitation.pug',
+			hostname: config.server.hostname
+		};
+		return userInvServicePersistence.inviteToCreateAccount(contactId, assignedRoles, configObj).asCallback(callback);
+	};
+
+	// return new UserInvService(userServicePersistence, partyServicePersistence, commServicePersistence, userInvServicePersistence);
+	return new UserInvService();
 };
 
 module.exports.create = function (UserService, PartyService, CommService, UserInvitationService) {

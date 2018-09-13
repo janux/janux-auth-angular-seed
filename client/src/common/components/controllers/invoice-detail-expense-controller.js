41cdfb3923a44bd074e494aa03101a4649331b95
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-09-13 12:51:55 -0500

    Workaround in order to show the invoice detail content not too cramped.

--- client/src/common/components/controllers/invoice-detail-expense-controller.js
+++ client/src/common/components/controllers/invoice-detail-expense-controller.js
@@ -89,7 +89,7 @@ module.exports =
 			suppressRowClickSelection: true,
 			rowSelection             : 'multiple',
 			animateRows              : true,
-			rowHeight                : 33,
+			rowHeight                : 30,
 			headerHeight             : 35,
 			enableSorting            : true,
 			pagination               : true,

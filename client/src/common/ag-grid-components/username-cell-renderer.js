2b3f7d869f7b6684d71755a168d3d8200bd12ba8
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-10-29 19:24:14 -0600

    Temporal fix for the blink screen in staff.

--- client/src/common/ag-grid-components/username-cell-renderer.js
+++ client/src/common/ag-grid-components/username-cell-renderer.js
@@ -1,15 +1,23 @@
 'use strict';
 
-function Username() {}
+var _ = require('lodash');
+
+function Username() {
+}
 
 Username.prototype.init = function (params) {
 	// console.log('user render params', params);
 	this.eGui = document.createElement('div');
-	this.eGui.align='center';
-	this.eGui.style.cssText='padding-top:0px;';
+	this.eGui.align = 'center';
+	this.eGui.style.cssText = 'padding-top:0px;';
 
 	var envelope = document.createElement('ag-grid-staff-invite');
 	envelope.setAttribute('staff-id', params.data.id);
+	envelope.setAttribute('staff-email-length',
+		_.isArray(params.data.contactMethods.emails) ?
+			_.filter(params.data.contactMethods.emails, function (o) {
+				return !_.isNil(o.address) && o.address !== '';
+			}).length.toString() : 0);
 
 	if (params.value !== '') {
 		this.eGui.innerText = params.value;

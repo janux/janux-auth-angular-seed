2931952ae7602f5ab05f990faaac45294fbcf4a8
Ernesto Ortiz Gómez <ernesto.tito@gmail.com>
2018-11-29 10:40:57 -0600

    Removed extra code.

--- client/src/app/services/view-consulting-controller.js
+++ client/src/app/services/view-consulting-controller.js
@@ -2,7 +2,7 @@
 
 var moment = require('moment');
 var _ = require('lodash');
-var agGridComp = require('common/ag-grid-components');
+// var agGridComp = require('common/ag-grid-components');
 
 
 module.exports =
@@ -11,9 +11,6 @@ module.exports =
 
 		console.debug('Operation', operation);
 
-		var formatStringOnlyHour = agGridComp.dateTimeCellEditor.formatStringOnlyHour;
-		var formatStringOnlyDate = agGridComp.dateTimeCellEditor.formatStringOnlyDate;
-		var columnsFiltersKey = config.jnxStoreKeys.specialOpsColumnsFilters;
 		var storedTab = jnxStorage.findItem('specialOpsViewSelectedTab', true);
 
 		$scope.cl = clientsList;
@@ -171,241 +168,11 @@ module.exports =
 		};
 
 
-		//
-		// Time sheet (AG-Grid)
-		//
-		var columnDefs = [
-			{
-				headerName  : $filter('translate')('operations.specialsTimeLog.staff'),
-				field       : 'staff',
-				editable    : false,
-				// cellRenderer: agGridComp.staffCellRenderer,
-				valueGetter : function (params) {
-					var res = params.data.staff.resource;
-					return nameQueryService.createLongNameLocalized(res);
-				},
-				cellEditor  : agGridComp.autocompleteStaffCellEditor,
-				filter      : 'agTextColumnFilter',
-				filterParams: {newRowsAction: 'keep'}
-			},
-			{
-				headerName    : $filter('translate')('operations.specialsTimeLog.function'),
-				field         : 'functionValue',
-				editable      : false,
-				cellEditor    : agGridComp.specialOpsFunctionCellEditor,
-				filter        : 'agTextColumnFilter',
-				filterParams  : {newRowsAction: 'keep'},
-				width         : 80,
-				valueFormatter: function (params) {
-					var locale;
-					if (!_.isNil(params.data)) {
-						switch (params.data.functionValue) {
-							case 'DRIVER':
-								locale = 'operations.specialsTimeLog.driver';
-								break;
-							case 'AGENT':
-								locale = 'operations.specialsTimeLog.agent';
-								break;
-							case 'AGENT_ARMED':
-								locale = 'operations.specialsTimeLog.agentArmed';
-								break;
-							case 'GREETER':
-								locale = 'operations.specialsTimeLog.greeter';
-								break;
-							case 'COORDINATOR':
-								locale = 'operations.specialsTimeLog.coordinator';
-								break;
-							case 'TRANSPORT':
-								locale = 'operations.specialsTimeLog.transport';
-								break;
-						}
-					}
-					if (!_.isNil(locale)) {
-						return $filter('translate')(locale);
-					} else {
-						return '';
-					}
-				}
-			},
-			{
-				headerName    : $filter('translate')('operations.specialsTimeLog.begin'),
-				field         : 'begin',
-				editable      : false,
-				filter        : 'date',
-				filterParams  : {
-					newRowsAction   : 'keep',
-					inRangeInclusive: true,
-					comparator      : agGridComp.dateFilterComparator,
-					filterOptions   : ['equals', 'notEqual', 'lessThan', 'lessThanOrEqual', 'greaterThan', 'greaterThanOrEqual', 'inRange']
-				},
-				valueFormatter: function (params) {
-					return (params.data.begin) ? moment(params.data.begin).format(formatStringOnlyDate) : '';
-				},
-				cellEditor    : agGridComp.dateTimeCellEditor,
-				sort          : 'desc',
-				width         : 160
-			},
-			{
-				headerName    : $filter('translate')('operations.specialsTimeLog.beginHour'),
-				field         : 'beginWork',
-				editable      : false,
-				filter        : 'date',
-				filterParams  : {
-					newRowsAction: 'keep',
-					comparator   : agGridComp.dateFilterComparator
-				},
-				valueFormatter: function (params) {
-					return (params.data.beginWork) ? moment(params.data.beginWork).format(formatStringOnlyHour) : '';
-				},
-				cellEditor    : agGridComp.dateTimeCellEditor,
-				width         : 160
-			},
-			{
-				headerName    : $filter('translate')('operations.specialsTimeLog.endHour'),
-				field         : 'endWork',
-				editable      : false,
-				filter        : 'date',
-				filterParams  : {
-					newRowsAction: 'keep',
-					comparator   : agGridComp.dateFilterComparator
-				},
-				valueFormatter: function (params) {
-					return (params.data.endWork) ? moment(params.data.endWork).format(formatStringOnlyHour) : '';
-				},
-				cellEditor    : agGridComp.dateTimeCellEditor,
-				width         : 160
-			},
-
-			{
-				headerName : $filter('translate')('operations.specialsTimeLog.duration'),
-				field      : 'duration',
-				editable   : false,
-				width      : 95,
-				valueGetter: function (params) {
-					return operationService.calculateDuration(params.data.beginWork, params.data.endWork);
-				}
-			},
-			{
-				headerName: $filter('translate')('operations.specialsTimeLog.comment'),
-				field     : 'comment',
-				editable  : false,
-				cellEditor: agGridComp.commentCellEditor,
-				cellStyle : {
-					'white-space': 'normal',
-					'padding-top': '0'
-				}
-			},
-			{
-				headerName : $filter('translate')('operations.specialsTimeLog.invoiceNumber'),
-				editable   : false,
-				hide       : !$rootScope.userRole.can('READ', 'FINANCE'),
-				valueGetter: function (params) {
-					var result = '';
-					if (!_.isNil(params.data.invoiceInfo) && !_.isNil(params.data.invoiceInfo.invoice)) {
-						result = params.data.invoiceInfo.invoice.invoiceNumber;
-					}
-					return result;
-				}
-			},
-			{
-				headerName             : '',
-				headerCheckboxSelection: true,
-				// headerCheckboxSelectionFilteredOnly: true,
-				// checkboxSelection: true,
-				cellRenderer           : agGridComp.checkBoxRowSelection,
-				// cellEditor             : agGridComp.rowActions,
-				// headerComponent: agGridComp.deleteRowsHeaderComponent,
-				editable               : true,
-				field                  : 'selected',	// field needed to avoid ag-grid warning
-				width                  : 45
-			}
-		];
-
-		var refreshing = false;		// Flag to indicate if ag-grid is in refresh process
-		var agGridSizeToFit = function () {
-			$timeout(function () {
-				$scope.gridOptions.api.sizeColumnsToFit();
-			}, 2000);
-		};
-		$scope.agGridSizeToFit = agGridSizeToFit;
-
-		// Only init grid if necessary
-		$scope.gridOptions = {
-			columnDefs               : columnDefs,
-			editType                 : 'fullRow',
-			rowData                  : [],
-			enableFilter             : true,
-			angularCompileRows       : true,
-			enableColResize          : true,
-			suppressRowClickSelection: true,
-			rowSelection             : 'multiple',
-			animateRows              : true,
-			rowHeight                : 45,
-			headerHeight             : 35,
-			enableSorting            : true,
-			pagination               : true,
-			paginationAutoPageSize   : true,
-			onGridReady              : function () {
-				agGridSizeToFit();
-
-				// Restore filter model.
-				var filterModel = jnxStorage.findItem(columnsFiltersKey, true);
-				if (!_.isNil(filterModel)) {
-					$scope.gridOptions.api.setFilterModel(filterModel);
-					$scope.gridOptions.onFilterChanged();
-				}
-			},
-
-			getRowHeight: function (rowObj) {
-				// assuming 50 characters per line, working how how many lines we need
-				return 18 * (Math.floor(rowObj.data.comment.length / 15) + 1);
-			},
-
-			onRowEditingStarted: function (rowObj) {
-				// Nothing to do yet
-				console.debug('Row edition started', rowObj);
-				$rootScope.$emit(config.timeEntry.specialOps.events.setUpdateMode, rowObj.data);
-				// rowObj.node.setRowHeight('45');
-				// $scope.gridOptions.api.onRowHeightChanged();
-			},
-
-			onRowValueChanged: function () {
-			},
-			localeTextFunc   : function (key, defaultValue) {
-				var gridKey = 'grid.' + key;
-				var value = $filter('translate')(gridKey);
-				return value === gridKey ? defaultValue : value;
-			},
-			onFilterChanged  : function () {
-				// Save filters to local storage.
-				var savedFilters;
-				savedFilters = $scope.gridOptions.api.getFilterModel();
-				jnxStorage.setItem(columnsFiltersKey, savedFilters, true);
-				// console.debug('savedFilters' + JSON.stringify(savedFilters));
-			},
-			onRowSelected    : function () {
-				console.debug('Event onRowSelected');
-				// Only one refresh at a time
-				if (!refreshing) {
-					refreshing = true;
-					$timeout(function () {
-						$scope.gridOptions.api.sizeColumnsToFit();
-						refreshing = false;
-					}, 200);
-				}
-			}
-		};
-
-
-		$scope.$on('sideMenuSizeChange', function () {
-			agGridSizeToFit();
-		});
-
 		// We need to reload because when the language changes ag-grid doesn't reload by itself
-		$rootScope.$on('$translateChangeSuccess', function () {
-			console.debug('$translateChangeSuccess');
-			$state.reload();
-		});
+		// $rootScope.$on('$translateChangeSuccess', function () {
+		// 	console.debug('$translateChangeSuccess');
+		// 	$state.reload();
+		// });
 
 		/**
 		 * This event is called when ..
